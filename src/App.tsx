8b5a96bb7c0a28495671244bb29730a702b1b10d
Five <1874803565@qq.com>
2021-07-02 18:31:13 +0800

    feat: 增加全局style功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
 
 import { lazy, Suspense } from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 const Recommend = lazy(() => import('./pages/recommend/index'))
 const Header = lazy(() => import('./components/header/index'))
 
+const theme = {
+  // 颜色定义规范
+  $colorBackground: '#222',
+  $colorBackgroundD: 'rgba(0, 0, 0, 0.3)',
+  $colorHighlightBackground: '#333',
+  $colorDialogBackground: '#666',
+  $colorTheme: '#ffcd32',
+  $colorThemeD: 'gba(255, 205, 49, 0.5)',
+  $colorSubTheme: '#d93f30',
+  $colorText: '#fff',
+  $colorTextD: 'rgba(255, 255, 255, 0.3)',
+  $colorTextL: 'rgba(255, 255, 255, 0.5)',
+  $colorTextLl: 'rgba(255, 255, 255, 0.8)',
+
+  // 字体定义规范
+  $fontSizeSmallS: '10px',
+  $fontSizeSmall: '12px',
+  $fontSizeMedium: '14px',
+  $fontSizeMediumX: '16px',
+  $fontSizeLarge: '18px',
+  $fontSizeLargeX: '22px',
+
+  // 基础功能
+
+  // $bgImage: ($url: string) => {
+  //   return {
+  //     backgroundImage: url($url + '@2x.png')
+  //   }
+  // }
+}
+
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>loading...</div>}>
-        <Header />
-        <HashRouter>
-          <Switch>
-            <Route path="/" component={Recommend} />
-            <Route path="/recommend" component={Recommend} />
-          </Switch>
-        </HashRouter>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<div>loading...</div>}>
+          <Header />
+          <HashRouter>
+            <Switch>
+              <Route path="/" component={Recommend} />
+              <Route path="/recommend" component={Recommend} />
+            </Switch>
+          </HashRouter>
+        </Suspense>
+      </ThemeProvider>
     </div>
   )
 }
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,8 +1,35 @@
 import styled from 'styled-components'
 
 export const DivAlias = styled.div`
-  color: red;
-  .text {
-    color: cyan;
-  }
+  height: 44px;
+    text-align: center;
+    color: ${(props: any) => props.theme.$colorTheme};
+    font-size: 0;
+    .icon {
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 6px;
+      width: 30px;
+      height: 32px;
+      margin-right: 9px;
+      @include bg-image('logo');
+      background-size: 30px 32px;
+    }
+    .text {
+      display: inline-block;
+      vertical-align: top;
+      line-height: 44px;
+      font-size: ${(props: any) => props.theme.$fontSizeLarge};
+    }
+    .mine {
+      position: absolute;
+      top: 0;
+      right: 0;
+      .icon-mine {
+        display: block;
+        padding: 12px;
+        font-size: ${(props: any) => props.theme.$fontSizeLargeX};
+        color: ${(props: any) => props.theme.$colorTheme};
+      }
+    }
 `
