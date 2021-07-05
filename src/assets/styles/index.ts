4b5bc96c83f8553f69226b97fc5b012001e8f2c4
Five <1874803565@qq.com>
2021-07-05 18:25:59 +0800

    feat: 增加基础项目配置

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,48 @@
+export const theme = {
+  // 颜色定义规范
+  $colorBackground: '#222',
+  $colorBackgroundD: 'rgba(0, 0, 0, 0.3)',
+  $colorHighlightBackground: '#333',
+  $colorDialogBackground: '#666',
+  $colorTheme: '#ffcd32',
+  $colorThemeD: 'gba(255, 205, 49, 0.5)',
+  $colorSubTheme: '#d93f30',
+  $colorText: '#fff',
+  $colorTextD: 'rgba(255, 255, 255, 0.3)',
+  $colorTextL: 'rgba(255, 255, 255, 0.5)',
+  $colorTextLl: 'rgba(255, 255, 255, 0.8)',
+
+  // 字体定义规范
+  $fontSizeSmallS: '10px',
+  $fontSizeSmall: '12px',
+  $fontSizeMedium: '14px',
+  $fontSizeMediumX: '16px',
+  $fontSizeLarge: '18px',
+  $fontSizeLargeX: '22px'
+}
+
+// 基础功能
+export const $bgImage = (url: string) => `
+    background-image: url(${url}});
+    @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+      background-image: url(${url}});
+    }
+  `
+// 禁止折行
+export const $noWrap = () => `
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`
+// 拓展小图标按钮的点击区域
+export const $extendClick = () => `
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: -10;
+    left: -10;
+    right: -10;
+    bottom: -10;
+  }
+`
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,35 +1,38 @@
 import styled from 'styled-components'
+import { $bgImage } from '~/assets/styles'
+import logo from '~/assets/images/logo@2x.png'
+console.log(logo)
 
 export const DivAlias = styled.div`
   height: 44px;
-    text-align: center;
-    color: ${(props: any) => props.theme.$colorTheme};
-    font-size: 0;
-    .icon {
-      display: inline-block;
-      vertical-align: top;
-      margin-top: 6px;
-      width: 30px;
-      height: 32px;
-      margin-right: 9px;
-      @include bg-image('logo');
-      background-size: 30px 32px;
+  text-align: center;
+  color: ${(props: any) => props.theme.$colorTheme};
+  font-size: 0;
+  .icon {
+    display: inline-block;
+    vertical-align: top;
+    margin-top: 6px;
+    width: 30px;
+    height: 32px;
+    margin-right: 9px;
+    ${$bgImage(logo)}
+    background-size: 30px 32px;
+  }
+  .text {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 44px;
+    font-size: ${(props: any) => props.theme.$fontSizeLarge};
+  }
+  .mine {
+    position: absolute;
+    top: 0;
+    right: 0;
+    .icon-mine {
+      display: block;
+      padding: 12px;
+      font-size: ${(props: any) => props.theme.$fontSizeLargeX};
+      color: ${(props: any) => props.theme.$colorTheme};
     }
-    .text {
-      display: inline-block;
-      vertical-align: top;
-      line-height: 44px;
-      font-size: ${(props: any) => props.theme.$fontSizeLarge};
-    }
-    .mine {
-      position: absolute;
-      top: 0;
-      right: 0;
-      .icon-mine {
-        display: block;
-        padding: 12px;
-        font-size: ${(props: any) => props.theme.$fontSizeLargeX};
-        color: ${(props: any) => props.theme.$colorTheme};
-      }
-    }
+  }
 `
